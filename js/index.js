11618ed1318a848cb820d13da2abc6ad1ad40873
Aron Schalk <aschalk@competa.com>
2019-02-04 13:37:37 +0100

    Updated sale-links-add.html & several styling changes

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 import NavToggle from './nav.js';
 import ModalToggle from './modal.js';
 import PreviewToggle from './preview-edit.js';
+import DragDropToggle from './drag-drop.js';
 
 const nav = new NavToggle();
 const modal = new ModalToggle();
 const preview = new PreviewToggle();
+const dragdrop = new DragDropToggle();
