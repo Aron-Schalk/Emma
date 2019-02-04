330ff0919ad3020a5314f3a0682afdde261afbbd
Aron Schalk <aschalk@competa.com>
2019-02-04 14:37:24 +0100

    Made navigation sticky

--- js/index.js
+++ js/index.js
@@ -1,9 +1,7 @@
 import NavToggle from './nav.js';
 import ModalToggle from './modal.js';
 import PreviewToggle from './preview-edit.js';
-import DragDropToggle from './drag-drop.js';
 
 const nav = new NavToggle();
 const modal = new ModalToggle();
 const preview = new PreviewToggle();
-const dragdrop = new DragDropToggle();
